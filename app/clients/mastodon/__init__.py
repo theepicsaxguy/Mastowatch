be268fd0df405085bdb5e5159cf74e11d8da89d2
theepicsaxguy <benjamin.sanden@hotmail.com>
2025-08-04 20:50:49 +0200

    removed api generated

--- app/clients/mastodon/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing Mastodon API"""
-
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- app/clients/mastodon/models/domain_block_severity.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class DomainBlockSeverity(str, Enum):
-    SILENCE = "silence"
-    SUSPEND = "suspend"
-
-    def __str__(self) -> str:
-        return str(self.value)
